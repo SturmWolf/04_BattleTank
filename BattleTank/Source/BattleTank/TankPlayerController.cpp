b2382ba406d975fa8c6acff0532d0ab1f1886855
sturmwolf <uberwolf@outlook.com>
2017-10-15 15:29:01 -0700

    Add Tick() to PlayerController.

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -18,6 +18,12 @@ void ATankPlayerController::BeginPlay()
 	}
 }
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const 
 {
 
@@ -25,6 +31,13 @@ ATank* ATankPlayerController::GetControlledTank() const
 
 }
 
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
 
+	// Get world location if linetrace through crosshair
+	// if it hits the landscape
+		// tell controlled tank to aim at this point
+}
 
 
