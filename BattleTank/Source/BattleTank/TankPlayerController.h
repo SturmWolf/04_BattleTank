e41c9108d67d6a33f422897baaa51be3d9a4750c
sturmwolf <uberwolf@outlook.com>
2017-10-15 03:16:08 -0700

    Packaged BTG/Fixed BP's/Created Tank&PlayerController C++ Classes

---
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Tank.H"
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h" // Must be last include
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ATank* GetControllerTank() const;
+	
+	
+	
+	
+};
