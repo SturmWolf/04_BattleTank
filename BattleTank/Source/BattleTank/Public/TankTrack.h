f57c70417bf26f009cec02a45867813f332a6ee3
sturmwolf <uberwolf@outlook.com>
2017-10-27 22:57:59 -0700

    ApplyForceAtLocaction()

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -17,6 +17,9 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 public:
 	UFUNCTION(BluePrintCallable, Category = Input)
 		void SetThrottle(float Throttle);
-	
+
+	// Max force per track in newwtons
+	UPROPERTY(EditDefaultsOnly)
+		float TrackMaxDrivingForce = 40000; // Asusume 40 ton tank, and ig acceleration
 	
 };
