619cd48350d0d13fa1e2d331efe19e5bb6ed8d0e
sturmwolf <uberwolf@outlook.com>
2017-10-28 00:47:22 -0700

    Fly-by-Wire Control System NavMovementComponent C++

---
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -0,0 +1,7 @@
+// Ben Wolfenbarger Copyright 2017
+
+#include "TankMovementComponent.h"
+
+
+
+
---
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -0,0 +1,20 @@
+// Ben Wolfenbarger Copyright 2017
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/NavMovementComponent.h"
+#include "TankMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
